c0034c66d59aceb4f12587477ee5557281c2896f
Gizyx <katha003@osloskolen.no>
2022-05-10 15:12:21 +0200

    Update!

--- TerminOppgave-DrawIT/php/support.php
+++ TerminOppgave-DrawIT/php/support.php
@@ -15,8 +15,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
     <title>Support</title>
 </head>
 
@@ -39,5 +40,32 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             </div>
         </div>
     </nav>
+    <section class="support">
+        <div>
+            <h1>How to-</h1>
+            <h3>Draw?</h3>
+            <p>While hovering over the canvas, simply click your left mouse button and drag.</p>
+            <h3>Erase?</h3>
+            <p>To erase follow the steps above while clicking right instead. </p>
+            <h3>Change size?</h3>
+            <p>Click and drag the slider on the left side of the screen</p><input type=" range" min="1" value="10">
+            <h3>Change color?</h3>
+            <p>The button below changes the color of your brush</p><input type="color"></td>
+            <h3></h3>
+        </div>
+        <div>
+            <h1>Bug repport or extra questions</h1>
+            <form>
+                <label for="email">Mail: </label><br>
+                <input type="text" name="email"><br>
+
+                <label for="problem">Question/bug:</label><br>
+                <textarea class="supportMsg" name="problem"></textarea><br>
+
+                <input type='submit'>
+            </form>
+        </div>
+    </section>
+</body>
 
 </html>
