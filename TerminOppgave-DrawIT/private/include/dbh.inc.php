da67399ffb6eb81e5e9bc79ab1274f08220b856d
Gizyx <katha003@osloskolen.no>
2022-05-11 13:14:24 +0200

    newLoginSys

---
+++ TerminOppgave-DrawIT/private/include/dbh.inc.php
@@ -0,0 +1,20 @@
+<?php
+/*Database credentials and initializing*/
+
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'drawit');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$databasename = "drawit";
+
+/*Attempt mySQL databse connection*/
+$conn = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+
+//Check connction
+if (mysqli_connect_error()) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "";
---
+++ TerminOppgave-DrawIT/public/signup.php
@@ -0,0 +1,41 @@
+<?php include_once 'header.php';?>
+
+<div class="formContainer">
+    <form action="../private/include/signup.inc.php" method="post">
+        <div></div>
+        <div id="title">Signup</div>
+        <input type="text" name="name" placeholder="Full name...">
+        <input type="text" name="email" placeholder="Email...">
+        <input type="text" name="uid" placeholder="Username...">
+        <input type="password" name="pwd" placeholder="Password...">
+        <input type="password" name="pwdrepeat" placeholder="repeat password...">
+        <button type="submit" name="submit">Sign in</button>
+    </form>
+    <?php 
+            if (isset($_GET["error"])) {
+                if($_GET["error"] == "emptyinput") {
+                    echo "<p>Fille in all fields</p>";
+                }
+                if($_GET["error"] == "invaliduid") {
+                    echo "<p>Choose a proper username</p>";
+                }
+                if($_GET["error"] == "invalidemail") {
+                    echo "<p>Choose a proper email</p>";
+                }
+                if($_GET["error"] == "passwordsdontmatch") {
+                    echo "<p>Password doesn't match</p>";
+                }
+                if($_GET["error"] == "stmtfailed") {
+                    echo "<p>Something went wrong, try again</p>";
+                }
+                if($_GET["error"] == "usernametaken") {
+                    echo "<p>Username already taken</p>";
+                }
+                if($_GET["error"] == "none") {
+                    echo "<p>You have signed up</p>";
+                }
+            }
+        ?>
+</div>
+
+<?php include_once 'footer.php' ?>
